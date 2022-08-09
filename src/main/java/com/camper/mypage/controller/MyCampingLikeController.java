68b46f4e403d38004795b61c3240b0de820d66af
hyeon.seung-jin <hhjab0206@gmail.com>
2022-08-09 09:40:24 +0900

    인터셉터 추가

--- src/main/java/com/camper/mypage/controller/MyCampingLikeController.java
+++ src/main/java/com/camper/mypage/controller/MyCampingLikeController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,7 +23,7 @@ public class MyCampingLikeController {
 	
 	// 캠핑장 찜 목록
 	// by. 승진 2022-08-08
-	@RequestMapping(value = "/myCampingLikeList.go")
+	@RequestMapping(value = "/myCampingLikeList.go", method = RequestMethod.GET)
 	public ModelAndView myCampingLikeList(HttpSession session) {
 		// String loginId = (String) session.getAttribute("loginId");
 		String temporaryId = "jin";
@@ -30,7 +32,7 @@ public class MyCampingLikeController {
 	
 	// 캠핑장 찜해제
 	// by. 승진 2022-08-08
-	@RequestMapping(value = "/myCampingLikeDelete.do")
+	@RequestMapping(value = "/myCampingLikeDelete.do", method = RequestMethod.GET)
 	public ModelAndView myCampingLikeDelete(@RequestParam String idx) {
 		// String loginId = (String) session.getAttribute("loginId");
 		String temporaryId = "jin";
