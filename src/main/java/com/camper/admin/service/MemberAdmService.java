bd84e92f5e2fa5fba8d713bab028550ebbb606a8
rocoa1 <dleoghks333@naver.com>
2022-08-17 17:51:27 +0900

    페이징 완료

--- src/main/java/com/camper/admin/service/MemberAdmService.java
+++ src/main/java/com/camper/admin/service/MemberAdmService.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.camper.admin.dao.MemberAdmDAO;
 import com.camper.admin.dto.MemberAdmDTO;
+import com.camper.lib.dto.PageMakerDTO;
 import com.camper.lib.utils.Criteria;
 
 @Service
@@ -19,11 +21,6 @@ public class MemberAdmService {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	public ArrayList<MemberAdmDTO> memberAdmList(HashMap<String, Object> params) {
-		
-		return dao.memberAdmList(params);
-	}
-
 	public MemberAdmDTO memberAdmDetail(String mb_id) {
 		
 		return dao.memberAdmDetail(mb_id);
@@ -32,6 +29,50 @@ public class MemberAdmService {
 	public int memberTotal(HashMap<String, Object> params) {
 		
 		return dao.memberTotal(params);
+	}
+
+	public ModelAndView memberAdmList(Criteria cri, HashMap<String, Object> params) {
+		ModelAndView mav = new ModelAndView("admin/memberAdmList");
+		
+		cri.setAmount(5);
+		if(params.get("keyword") != null && !params.get("keyword").toString().trim().equals("")) {
+			cri.setKeyword((String) params.get("keyword"));
+			cri.setType((String) params.get("type"));
+			
+			
+			mav.addObject("keyword", params.get("keyword"));
+			mav.addObject("type", params.get("type"));
+		}
+		 // int i= (int) params.get("mb_id");
+		int total = dao.memberTotal(params);
+		mav.addObject("listCnt", total);
+		
+		PageMakerDTO pageMaker = new PageMakerDTO(cri, total);
+		
+		int pageNum = cri.getPageNum();
+		
+		//현재 페이지가 마지막 페이지를 초과하지 못하도록 방지하는 코드
+		if(pageMaker.getEndPage() > 0 && pageNum > pageMaker.getEndPage()) {
+			pageNum = pageMaker.getEndPage();
+			cri.setPageNum(pageNum);
+				}
+				
+		//DAO MAPPER OFFSET
+		int skip = (pageNum - 1) * cri.getAmount();
+			params.put("skip", skip);
+			mav.addObject("skip", skip);
+				
+		//DAO MAPPER LIMIT
+		params.put("amount", cri.getAmount());
+				
+		
+		ArrayList<MemberAdmDTO> list = dao.memberAdmList(params);
+		logger.info("list 갯수 : "+list.size());
+		mav.addObject("list", list);
+		
+		mav.addObject("pageMaker", pageMaker);
+		
+		return mav;
 	}
 
 	
